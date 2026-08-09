dae506472d491151411b27e94f86819a6a3bfd4e
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-08-09 23:20:43 +0530

    not found added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <div className="not-found-content">
+        <p className="not-found-label">ROGUE STUDIO</p>
+
+        <h1>Coming Soon</h1>
+
+        <p>
+          We&apos;re working on something new.
+          <br />
+          This page will be available soon.
+        </p>
+
+        <a href="/">Back to Home</a>
+      </div>
+    </main>
+  );
+}
